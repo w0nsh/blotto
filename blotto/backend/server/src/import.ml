ad9a97cc092ce15948503e519e60d32bc214cfac
Franciszek Malinka <franciszek.malinka@gmail.com>
2023-11-26 22:17:13 +0100

    Better logging.

--- blotto/backend/server/src/import.ml
+++ blotto/backend/server/src/import.ml
@@ -11,6 +11,7 @@ include struct
   module Remove_game = Remove_game
   module List_users = List_users
   module User_info = User_info
+  module Rpc_intf = Rpc_intf
   module Recalculate_scoreboard = Recalculate_scoreboard
 end
 
